c652a40d3e864e89401a249864e92ef100ed8de5
Daria <dariabatiuk5@gmail.com>
2023-08-24 23:33:09 -0300

    cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
-import { checkAuth, handleValidationErrors } from './utils/index.js';
-import { userController, postController} from './controllers/index.js';
-import multer from 'multer';
-import cors from 'cors';
-import fs from 'fs';
-
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+import { checkAuth, handleValidationErrors } from "./utils/index.js";
+import { userController, postController } from "./controllers/index.js";
+import multer from "multer";
+import cors from "cors";
+import fs from "fs";
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -16,46 +19,67 @@ mongoose
 const app = express();
 
 const storage = multer.diskStorage({
-	destination: (_, __, cb) =>{
-		if(!fs.existsSync('uploads')){
-			fs.mkdirSync('uploads');
-		}
-		cb(null, 'uploads');
-	},
-	filename: (_, file, cb) =>{
-		cb(null, file.originalname);
-	},
+  destination: (_, __, cb) => {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage });
 
 app.use(express.json());
-app.use(cors({
-  origin: 'https://blog-project-frontend-ijetzrtzz-dariabatiuk.vercel.app',
-  methods: 'GET,POST,PATCH,DELETE',
-  credentials: true, 
-}));
+app.use(
+  cors({
+    origin: ["https://blog-project-frontend-omega.vercel.app"],
+  })
+);
+app.use("/uploads", express.static("uploads"));
 
-app.use('/uploads', express.static('uploads'));
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  userController.login
+);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  userController.register
+);
 
-app.post("/auth/login", loginValidation, handleValidationErrors, userController.login);
-app.post("/auth/register", registerValidation,  handleValidationErrors,userController.register);
-
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-	res.json({
-		url: `/uploads/${req.file.originalname}`,
-	});	
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
 });
 
-app.get('/auth/me', checkAuth, userController.getMe);
-app.get('/posts', postController.getAll);
-app.get('/posts/:id', postController.getOne);
-app.post('/posts/', checkAuth, postCreateValidation, handleValidationErrors, postController.create);
-app.delete('/posts/:id', checkAuth, postController.remove);
-app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, postController.update);
+app.get("/auth/me", checkAuth, userController.getMe);
+app.get("/posts", postController.getAll);
+app.get("/posts/:id", postController.getOne);
+app.post(
+  "/posts/",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  postController.create
+);
+app.delete("/posts/:id", checkAuth, postController.remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  postController.update
+);
 
-app.get('/tags', postController.getLastTags);
-app.get('/posts/tags', postController.getLastTags);
+app.get("/tags", postController.getLastTags);
+app.get("/posts/tags", postController.getLastTags);
 
 app.listen(process.env.PORT || 444, (err) => {
   if (err) {
